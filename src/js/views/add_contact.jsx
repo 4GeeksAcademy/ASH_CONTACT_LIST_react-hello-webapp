531ca85f770aa4cf3c3515718e58ab87aeca5f66
agusotoholt <168455366+agusotoholt@users.noreply.github.com>
2024-07-12 01:42:08 +0000

    creado contact form componente

--- src/js/views/add_contact.jsx
+++ src/js/views/add_contact.jsx
@@ -1,37 +1,11 @@
 import React, {useState, useEffect, useContext} from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext.js";
+import ContactForm from "../component/contact_form.jsx";
 
     const AddContact = () => {
-        const {store, actions} = useContext(Context);
-        const [name, setName] = useState("");
-        const [email, setEmail] = useState("");
-        const [phone, setPhone] = useState("");
-        const [address, setAddress] = useState("");
-        const navigate = useNavigate();
-
-    const handleKeyDown = (e, setter) => {
-        if (e.key === "Enter") {
-            e.target.value === "" ? alert("el campo esta vacio, tontin") : setter(e.target.value);
-            e.preventDefault();
-            }};
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (!name || !email || !phone || !address) {
-            alert("Please fill in all fields");
-            return;
-        }
-        actions.crearContacto(name, phone, email, address);
-        setName("");
-        setEmail("");
-        setPhone("");
-        setAddress("");
-        navigate("/");
-        };
         
     useEffect (() => {
-        // actions.crearContacto();
         // actions.obtenerContactos();
     }, [])
 
@@ -40,33 +14,7 @@ import { Context } from "../store/appContext.js";
             <div className="container-fluid m-5 d-flex justify-content-center">
                 <h1>Add a new contact</h1>
             </div>
-            <div className="d-flex justify-content-center">
-            <form className="container-fluid" onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputName" className="form-label">Full Name</label>
-                    <input type="text" className="form-control" placeholder="Enter Full Name" id="exampleInputName" value={name} onChange={(e)=>setName(e.target.value)} onKeyDown={(e)=>handleKeyDown(e, setName)} />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter Email" id="exampleInputEmail1" value={email} onChange={(e)=>setEmail(e.target.value)} onKeyDown={(e)=>handleKeyDown(e, setEmail)} />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputPhone" className="form-label">Phone</label>
-                    <input type="text" className="form-control" placeholder="Enter Phone" id="exampleInputPhone" value={phone} onChange={(e)=>setPhone(e.target.value)} onKeyDown={(e)=>handleKeyDown(e, setPhone)} />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputAddress" className="form-label">Address</label>
-                    <input type="text" className="form-control" placeholder="Enter Address" id="exampleInputAddress" value={address} onChange={(e)=>setAddress(e.target.value)} onKeyDown={(e)=>handleKeyDown(e, setAddress)} />
-                </div>
-                <div className="container-fluid m-0 p-0">
-                    <button type="submit" className="btn btn-primary w-100 m-0 p-0">Save</button>
-                </div>
-                <div className="container-fluid d-flex justify-content-start px-0">
-                    <Link to="/" className="btn btn-sm btn-link px-0">Back to contacts</Link>
-                </div>
-
-            </form>
-            </div>
+            <ContactForm />
         </div>
 
     )
